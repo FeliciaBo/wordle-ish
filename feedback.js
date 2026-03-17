1cc096e468d64b712d32602383d3938bd7869add
FeliciaBo <theskylla@gmail.com>
2026-03-17 07:59:31 +0100

    start of feedback code

--- feedback.js
+++ feedback.js
@@ -1,1 +1,11 @@
 
+function feedback (guess, correctWord) {
+
+const result = [];
+const correctLetters = []; //array made of correct word
+
+
+}
+
+
+// itterera genom för att hitta - letter: incorrect | misplaced | correct
