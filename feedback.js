f0b0949cb2f3d05ac391b0e6ea2ffbc23e68cd1e
FeliciaBo <theskylla@gmail.com>
2026-03-18 16:16:45 +0100

    Added test and functionality for correct guess

--- feedback.js
+++ feedback.js
@@ -1,21 +1,34 @@
 
-function feedback (guess, correctWord) {
+function feedback (guess, correct) {
 
-//const result = [];
-//const correctLetters = correctWord.split('');
+const result = [];
+//const letter = correct.split('');
 
+//error handling
 if (!guess) {
  throw new Error("Input cannot be empty")
 }
 
-if (guess.length !== correctWord.length) {
+if (guess.length !== correct.length) {
  throw new Error("Your guess must be the same lenght as the correct word")
 }
+
 if (!/^[A-Za-zÅÄÖåäö]+$/.test(guess)) {
     throw new Error("Your guess can only contain letters");
   }
 
+  // itterera genom för att hitta - letter: incorrect | misplaced | correct
+
+
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === correct[i]) {
+      result.push(guess[i] + ": correct");
+    
+  }}
+
+return result;
+
 }
 
+
 module.exports = feedback;
-// itterera genom för att hitta - letter: incorrect | misplaced | correct
--- feedback.test.js
+++ feedback.test.js
@@ -8,13 +8,28 @@ const feedback = require('./feedback');
 - Testa ord med dubletter av bokstäver: "hallå" och "cykla"
 */
 
+describe("Check output for different answers", () => {
+
+  test("Correct guess - all letters correct", () => {
+    const result = feedback("CYKLA", "CYKLA");
+    expect(result).toEqual([
+      "C: correct",
+      "Y: correct",
+      "K: correct",
+      "L: correct",
+      "A: correct"
+    ]);
+  });
+
+});
+
+
 /*Felhantering:
 - Testa tom input
 - Testa för kort och för lång input
 - Testa input med specialtecken och siffror
 */
 
- 
 describe("Trow error messages for wrong input", () => {
 
     test("Empty guess", () => {
@@ -26,7 +41,6 @@ describe("Trow error messages for wrong input", () => {
    test("Too short guess", () => {
     expect(() => feedback("HEJ", "CYKLA"))
     .toThrow("Your guess must be the same lenght as the correct word");
-
    });
 
    test("Too long guess", () => {
