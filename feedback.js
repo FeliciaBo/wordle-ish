522005b36918d870708658477f0a9be62b805e5f
FeliciaBo <theskylla@gmail.com>
2026-03-18 13:41:31 +0100

    Added jest, created test for empty input (guess). Removed empty test file for now

--- feedback.js
+++ feedback.js
@@ -1,11 +1,14 @@
 
 function feedback (guess, correctWord) {
 
-const result = [];
-const correctLetters = correctWord.split('');
+//const result = [];
+//const correctLetters = correctWord.split('');
 
+if (!guess) {
+ throw new Error("Input cannot be empty")
+}
 
 }
 
-
+module.exports = feedback;
 // itterera genom för att hitta - letter: incorrect | misplaced | correct
--- feedback.test.js
+++ feedback.test.js
@@ -1,13 +1,25 @@
+const feedback = require('./feedback');
+
+
 
 /* Testsenarion:
 - Testa rätt svar
 - Testa med ord som ger: incorrect | misplaced | correct
 - Testa ord med dubletter av bokstäver: "hallå" och "cykla"
 */
+ 
+describe("Error handling for feedback", () => {
 
+    test("Trow errror message for empty guess", () => {
+    expect(() => feedback("", "CYKLA"))
+    .toThrow("Input cannot be empty");
+
+   });
+  });
 
 
 /*Felhantering:
+- Testa tom input
 - Testa för kort och för lång input
 - Testa input med specialtecken och siffror
 */
