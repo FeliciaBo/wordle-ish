36f97a67afd6679e4604d918d5e77dfd4eb56306
FeliciaBo <theskylla@gmail.com>
2026-03-19 15:27:40 +0100

    start of choose-word-algorithm

--- choose-word.js
+++ choose-word.js
@@ -1,1 +1,13 @@
 
+/* Inputs:
+ - En lista med ord
+- En siffra som anger önskad längd
+- En indikation på huruvida samma bokstav får förekomma mer 
+  än en gång i ordet, eller om alla bokstäver måste vara unika
+  */
+
+function chooseWord(word, wordLenght, unique) {
+
+}
+
+module.exports = chooseWord;
---
+++ choose-word.test.js
@@ -0,0 +1,16 @@
+/* Denna funktion är spelets sätt att välja ut ett ord 
+att spela med. Den ska uppfylla följande kriterier:
+
+Inputs:
+- En lista med ord
+- En siffra som anger önskad längd
+- En indikation på huruvida samma bokstav får förekomma mer 
+  än en gång i ordet, eller om alla bokstäver måste vara unika
+
+Funktionalitet:
+- Välj slumpmässigt ut ett ord ur listan som uppfyller 
+  kriterierna i de övriga parametrarna
+- Hantera på något väldefinierat sätt situationen 
+  som uppstår när inget passande ord finns
+- Output: Det slumpmässigt utvalda ordet */
+
